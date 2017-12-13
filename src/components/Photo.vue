58e6c4726d1a4e57eca1741664ab4ebc008cab92
mephyo <hergloves@gmail.com>
2017-12-13 18:03:35 +0800

    ja ja

---
+++ src/components/Photo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="photo">
+      <img :src="thumb" class="photo-thumb">
+      <img :src="src" class="photo-main" ref="mainPhoto">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Photo",
+  props: {
+    src: String,
+    thumb: String
+  },
+  watch: {
+      $mainPhoto(){
+          
+      }
+  }
+};
+</script>
