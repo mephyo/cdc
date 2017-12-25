2c4d356469590d87354da7a37336173c0b723458
mephyo <hergloves@gmail.com>
2017-12-25 13:29:34 +0800

    changed something

--- src/components/Stack.vue
+++ src/components/Stack.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="stack">
-    <img class="charlie" :img="gallery[2]">
-    <img class="bravo" :img="gallery[1]">
-    <img class="able" :img="gallery[0]">
+    <img class="charlie" :src="gallery[2]">
+    <img class="bravo" :src="gallery[1]">
+    <img class="able" :src="gallery[0]">
   </div>
 </template>
 
